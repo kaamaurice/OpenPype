b011fa180cdd01a4424dde60f67f47470cb991d6
Kaa Maurice <kaalium@gmail.com>
2022-08-23 11:53:34 +0200

    extract blend pack unpack fixed

--- openpype/hosts/blender/plugins/publish/extract_blend.py
+++ openpype/hosts/blender/plugins/publish/extract_blend.py
@@ -40,8 +40,8 @@ class ExtractBlend(openpype.api.Extractor):
                     return loader
         return loaders[0]
 
-    def _pack_images_from_objects(self, objects):
-        """Pack images from mesh objects materials."""
+    def _get_images_from_objects(self, objects):
+        """Get images from mesh objects materials."""
         # Get all objects materials using node tree shader.
         materials = set()
         for obj in objects:
@@ -61,10 +61,7 @@ class ExtractBlend(openpype.api.Extractor):
                     and node.image
                 ):
                     images.add(node.image)
-        # Pack images.
-        for image in images:
-            if not image.packed_file:
-                image.pack()
+        return images
 
     def process(self, instance):
         # Define extract output file path
@@ -119,11 +116,25 @@ class ExtractBlend(openpype.api.Extractor):
         )
 
         # Pack used images in the blend files.
+        packed_images = set()
         if self.pack_images:
-            self._pack_images_from_objects(objects)
+            for image in self._get_images_from_objects(objects):
+                if not image.packed_file:
+                    packed_images.add((image, image.is_dirty))
+                    image.pack()
 
         bpy.ops.file.make_paths_absolute()
         bpy.data.libraries.write(filepath, data_blocks)
+
+        # restor packed images.
+        for image, is_dirty in packed_images:
+            if not image.filepath:
+                unpack_method = "REMOVE"
+            elif is_dirty:
+                unpack_method = "WRITE_ORIGINAL"
+            else:
+                unpack_method = "USE_ORIGINAL"
+            image.unpack(method=unpack_method)
 
         # restor instance metadata
         instance_collection[AVALON_PROPERTY] = instance_metadata
