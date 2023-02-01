ff79762a8e439ad4eafd470fe31139d3343a05af
Félix David <felixg.david@gmail.com>
2023-02-01 18:19:14 +0100

    refactor with _get_camera_from_datablocks as shared function

--- openpype/hosts/blender/plugins/create/create_camera.py
+++ openpype/hosts/blender/plugins/create/create_camera.py
@@ -8,6 +8,29 @@ from openpype.hosts.blender.api import plugin
 from openpype.hosts.blender.api.properties import OpenpypeInstance
 from openpype.hosts.blender.api.utils import get_all_outliner_children
 
+def _get_camera_from_datablocks(datablocks: List[bpy.types.ID])->Union[bpy.types.Object, None]:
+    """Get first camera found in given datablocks list.
+
+    Args:
+        datablocks (List[bpy.types.ID]):
+            List of datablocks to get camera from
+
+    Returns:
+        Union[bpy.types.Object, None]: Found camera if any.
+    """
+    outliner_children = set(
+        chain.from_iterable(
+            get_all_outliner_children(d) for d in datablocks
+        )
+    )
+    return next(
+        (
+            c
+            for c in outliner_children | set(datablocks)
+            if isinstance(c, bpy.types.Object) and c.type == "CAMERA"
+        ),
+        None,
+    )
 
 class CreateCamera(plugin.Creator):
     """Single baked camera"""
@@ -16,31 +39,6 @@ class CreateCamera(plugin.Creator):
     label = "Camera"
     family = "camera"
     icon = "video-camera"
-
-    @staticmethod
-    def _get_camera_from_datablocks(datablocks: List[bpy.types.ID])->Union[bpy.types.Object, None]:
-        """Get first camera found in given datablocks list.
-
-        Args:
-            datablocks (List[bpy.types.ID]):
-                List of datablocks to get camera from
-
-        Returns:
-            Union[bpy.types.Object, None]: Found camera if any.
-        """
-        outliner_children = set(
-            chain.from_iterable(
-                get_all_outliner_children(d) for d in datablocks
-            )
-        )
-        return next(
-            (
-                c
-                for c in outliner_children | set(datablocks)
-                if isinstance(c, bpy.types.Object) and c.type == "CAMERA"
-            ),
-            None,
-        )
 
     def process(
         self, datablocks: List[bpy.types.ID] = None, **kwargs
@@ -52,7 +50,7 @@ class CreateCamera(plugin.Creator):
 
         # Rename existing camera or create one
         datablocks = datablocks or []
-        camera = self._get_camera_from_datablocks(datablocks)
+        camera = _get_camera_from_datablocks(datablocks)
         if camera:
             camera.name = instance_name
             camera.data.name = instance_name
