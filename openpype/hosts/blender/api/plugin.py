4307b829ae47ba76e55feb65e82afefbf0bfe2e6
Félix David <felixg.david@gmail.com>
2023-02-10 16:08:25 +0100

    Fix: action not user remapped

--- openpype/hosts/blender/api/plugin.py
+++ openpype/hosts/blender/api/plugin.py
@@ -1236,6 +1236,14 @@ class AssetLoader(Loader):
                     new_datablock.name = old_datablock["original_name"]
                     old_datablock.user_remap(new_datablock)
 
+                    # Ensure action remap, due to bug
+                    # https://projects.blender.org/blender/blender/issues/104576
+                    # TODO remove when fixed
+                    if hasattr(old_datablock, "animation_data") and old_datablock.animation_data:
+                        if not new_datablock.animation_data:
+                            new_datablock.animation_data_create()
+                        new_datablock.animation_data.action = old_datablock.animation_data.action
+
             # Restore parent collection if existing
             if parent_collection:
                 unlink_from_collection(
