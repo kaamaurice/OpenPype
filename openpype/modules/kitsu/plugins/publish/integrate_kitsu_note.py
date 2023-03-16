7d4047f33f49af8ba1969ffa96ea70ec9d49e673
Félix David <felixg.david@gmail.com>
2023-03-16 13:20:07 +0100

    fix publish each review instance only

--- openpype/modules/kitsu/plugins/publish/integrate_kitsu_note.py
+++ openpype/modules/kitsu/plugins/publish/integrate_kitsu_note.py
@@ -9,7 +9,7 @@ class IntegrateKitsuNote(pyblish.api.ContextPlugin):
 
     order = pyblish.api.IntegratorOrder - 0.01
     label = "Kitsu Note and Status"
-    families = ["render", "image", "online", "plate", "kitsu"]
+    families = ["render", "image", "online", "plate", "kitsu", "review"]
 
     # status settings
     set_status_note = False
