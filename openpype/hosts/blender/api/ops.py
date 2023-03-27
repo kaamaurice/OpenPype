c90f374c572f141076126322c4ca3b7e48ee1ef9
Félix David <felixg.david@gmail.com>
2023-03-27 10:59:10 +0200

    fix make_paths_absolute import

--- openpype/hosts/blender/api/ops.py
+++ openpype/hosts/blender/api/ops.py
@@ -33,6 +33,7 @@ from openpype.client.entities import (
     get_version_by_id,
     get_subset_by_id,
 )
+from openpype.hosts.blender.utility_scripts import make_paths_absolute
 from openpype.lib.path_tools import version_up
 from openpype.modules.sync_server.sync_server import (
     get_last_published_workfile_path,
