54aa8768f21d3dec86184841b4890ca024079eb7
Sharkitty <hayleylalicorne@gmail.com>
2023-04-25 17:41:05 +0200

    Changing implementation to load the same version

--- openpype/hosts/blender/plugins/inventory/remove_and_load.py
+++ openpype/hosts/blender/plugins/inventory/remove_and_load.py
@@ -12,7 +12,7 @@ from openpype.client import (
 class RemoveAndLoad(InventoryAction):
     """Delete inventory item and reload it."""
 
-    label = "Delete and load"
+    label = "Remove and load"
     icon = refresh
 
     def process(self, containers):
@@ -32,22 +32,22 @@ class RemoveAndLoad(InventoryAction):
                 "Failed to get loader, can't remove and load container",
             )
 
-            current_representation = containers["representation"]
-            assert current_representation, "Represenatation not found"
+            representation = containers["representation"]
+            assert representation, "Represenatation not found"
 
             # get repre by id?
 
             (
-                current_version,
+                version,
                 subset,
                 asset,
                 project,
             ) = get_representation_parents(
-                project_name, current_representation
+                project_name, representation
             )
 
             assert (
-                current_version and subset and asset and project
+                version and subset and asset and project
             ), "Failed to get representation parents"
 
             loader.remove(
@@ -58,27 +58,9 @@ class RemoveAndLoad(InventoryAction):
                     },
                     "asset": asset,
                     "subset": subset,
-                    "version": current_version,
-                    "representation": current_representation,
+                    "version": version,
+                    "representation": representation,
                 }
             )
 
-            # TODO Get is hero
-            is_hero = False
-            if is_hero:
-                new_version = get_hero_version_by_subset_id(
-                    project_name, current_version["parent"], fields=["_id"]
-                )
-            else:
-                new_version = get_last_version_by_subset_id(
-                    project_name, current_version["parent"], fields=["_id"]
-                )
-
-            new_representation = get_representation_by_name(
-                project_name,
-                current_representation["name"],
-                new_version["parent"],
-                fields=["_id"],
-            )
-
-            loader.update(container, new_representation)
+            loader.update(container, representation)
