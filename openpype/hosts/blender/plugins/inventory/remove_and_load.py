03bf3b39d8e08580cfeaaf19a6284ba743ed0939
Sharkitty <hayleylalicorne@gmail.com>
2023-04-27 18:57:21 +0200

    Fixed remove and load, blender inv action support

--- openpype/hosts/blender/plugins/inventory/remove_and_load.py
+++ openpype/hosts/blender/plugins/inventory/remove_and_load.py
@@ -1,27 +1,26 @@
 from openpype.pipeline import InventoryAction
 from openpype.pipeline.legacy_io import Session
 from openpype.pipeline.load.plugins import discover_loader_plugins
-from openpype.client import (
-    get_representation_parents,
-    get_representation_by_name,
-    get_last_version_by_subset_id,
-    get_hero_version_by_subset_id,
+from openpype.pipeline.load.utils import (
+    get_loader_identifier,
+    remove_container,
+    load_container,
 )
+from openpype.client import get_representation_by_id
 
 
 class RemoveAndLoad(InventoryAction):
     """Delete inventory item and reload it."""
 
     label = "Remove and load"
-    icon = refresh
+    icon = 'refresh'
 
     def process(self, containers):
-        # TODO Create Qt dialog window
         for container in containers:
             project_name = Session.get("AVALON_PROJECT")
 
+            # Get loader
             loader_name = container["loader"]
-            print(load_name)
             for plugin in discover_loader_plugins(project_name=project_name):
                 if get_loader_identifier(plugin) == loader_name:
                     loader = plugin
@@ -32,35 +31,14 @@ class RemoveAndLoad(InventoryAction):
                 "Failed to get loader, can't remove and load container",
             )
 
-            representation = containers["representation"]
+            # Get representation
+            representation = get_representation_by_id(
+                project_name, container["representation"]
+            )
             assert representation, "Represenatation not found"
 
-            # get repre by id?
+            # Remove container
+            remove_container(container)
 
-            (
-                version,
-                subset,
-                asset,
-                project,
-            ) = get_representation_parents(
-                project_name, representation
-            )
-
-            assert (
-                version and subset and asset and project
-            ), "Failed to get representation parents"
-
-            loader.remove(
-                {
-                    "project": {
-                        "name": project["name"],
-                        "code": project["data"].get("code", ""),
-                    },
-                    "asset": asset,
-                    "subset": subset,
-                    "version": version,
-                    "representation": representation,
-                }
-            )
-
-            loader.update(container, representation)
+            # Load container
+            load_container(loader, representation)
