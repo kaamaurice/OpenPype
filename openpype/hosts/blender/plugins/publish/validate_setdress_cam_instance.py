ec91ea5f37d434400fdf9a13192c37d0b62abca9
BlueLag <neveu.julien@hotmail.com>
2023-07-18 10:02:29 +0200

    fix: cleaner method with review and camera instances

--- openpype/hosts/blender/plugins/publish/validate_setdress_cam_instance.py
+++ openpype/hosts/blender/plugins/publish/validate_setdress_cam_instance.py
@@ -9,13 +9,13 @@ import openpype.hosts.blender.api.action
 from openpype.pipeline.publish import ValidateContentsOrder
 
 
-class ValidateSetdressCamInstance(pyblish.api.Validator):
-    """Validate that cam and review instances points on the right collection."""
+class ValidateReviewCamInstance(pyblish.api.Validator):
+    """Validate that cam and review instances point to the right collection."""
 
     order = ValidateContentsOrder
     hosts = ["blender"]
-    families = ["setdress"]
-    label = "Validate SetDress Cam Review Instances"
+    families = ["review", "camera"]
+    label = "Validate Cam Review Instances"
     actions = [openpype.hosts.blender.api.action.SelectInvalidAction]
     optional = False
 
@@ -23,27 +23,15 @@ class ValidateSetdressCamInstance(pyblish.api.Validator):
     def get_invalid(instance) -> List:
         invalid = []
 
-        # Get all openpype instances
-        all_instances = bpy.context.scene.openpype_instances
-
-        for inst in all_instances:
-            # Check only cameraMain and reviewMain instances
-            if inst.name.find("cameraMain") != -1 or inst.name.find("reviewMain") != -1:
-                datablocks = None
-                datablocks = inst.get_datablocks()
-                # Check if the datablocks set is not empty
-                if len(list(datablocks)) > 0:
-                    for datablock in datablocks:
-                        # Check if the datablock is not a collection
-                        if type(datablock) is not bpy.types.Collection and inst.name not in invalid:
-                            invalid.append(inst.name)
-                else:
-                    invalid.append(inst.name)
+        for obj in instance:
+            # Check if the object is not a collection
+            if not isinstance(obj, bpy.types.Collection) and instance not in invalid:
+                invalid.append(instance)
         return invalid
 
     def process(self, instance):
         invalid = self.get_invalid(instance)
         if invalid:
             raise RuntimeError(
-                f"Instance(s) {invalid} is/are not pointing on a collection."
+                f"Instance(s) {invalid} is/are not pointing to a collection."
             )
