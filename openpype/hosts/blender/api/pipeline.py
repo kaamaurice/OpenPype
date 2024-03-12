36c455cd2020abbe9e321540fdf7551175247629
Strangenoise <tristan.legranche@gmail.com>
2024-03-12 09:24:25 +0100

    Remove workfile up-to-date related code

--- openpype/hosts/blender/api/pipeline.py
+++ openpype/hosts/blender/api/pipeline.py
@@ -154,9 +154,6 @@ def set_use_file_compression():
 def on_new():
     set_start_end_frames()
     set_use_file_compression()
-    bpy.types.Scene.is_workfile_up_to_date = bpy.props.BoolProperty(
-        name="Is Workfile Up To Date",
-    )
     bpy.types.WindowManager.subset_is_locked = bpy.props.BoolProperty(
         name="Is subset locked",
     )
